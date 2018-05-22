e73649482ffe3ece076626207d3d1c5c0ae011ce
johnny ray alt and Matt Dowdney <johnny@>
2018-05-22 11:02:49 -0700

    add final test for scrabble

--- src/main/java/models/Scrabble.java
+++ src/main/java/models/Scrabble.java
@@ -5,13 +5,57 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class Scrabble {
+    List<Character> inputToArray = new ArrayList<Character>();
+    Map<Character, Integer> scrabbleScoreBank = new HashMap<Character, Integer>();
 
-    Map<String, Integer> scrabbleScoreBank = new HashMap<String, Integer>();
+    public Scrabble () {
+      scrabbleScoreBank.put('a', 1);
+      scrabbleScoreBank.put('e', 1);
+      scrabbleScoreBank.put('i', 1);
+      scrabbleScoreBank.put('o', 1);
+      scrabbleScoreBank.put('u', 1);
+      scrabbleScoreBank.put('l', 1);
+      scrabbleScoreBank.put('n', 1);
+      scrabbleScoreBank.put('r', 1);
+      scrabbleScoreBank.put('s', 1);
+      scrabbleScoreBank.put('t', 1);
+      scrabbleScoreBank.put('d', 2);
+      scrabbleScoreBank.put('g', 2);
+      scrabbleScoreBank.put('b', 3);
+      scrabbleScoreBank.put('c', 3);
+      scrabbleScoreBank.put('m', 3);
+      scrabbleScoreBank.put('p', 3);
+      scrabbleScoreBank.put('f', 4);
+      scrabbleScoreBank.put('h', 4);
+      scrabbleScoreBank.put('v', 4);
+      scrabbleScoreBank.put('w', 4);
+      scrabbleScoreBank.put('y', 4);
+      scrabbleScoreBank.put('k', 5);
+      scrabbleScoreBank.put('j', 8);
+      scrabbleScoreBank.put('x', 8);
+      scrabbleScoreBank.put('q', 10);
+      scrabbleScoreBank.put('z', 10);
+  }
+
+
+
+
+
+
 
     public Integer calculateScore(String input){
-      return null;
+      char[] splitArray = input.toLowerCase().toCharArray();
+        for (char letter: splitArray) {
+            Character wrappedLetter = letter;
+            inputToArray.add(wrappedLetter);
+        }
+        Integer total = 0;
+            for (Character singleLetter : inputToArray) {
+              total +=  scrabbleScoreBank.get(singleLetter);
+            }
+        return total;
+        }
     }
 
 
-}
 
--- src/test/java/models/ScrabbleTest.java
+++ src/test/java/models/ScrabbleTest.java
@@ -2,10 +2,10 @@ package models;
 
 import org.junit.*;
 import static org.junit.Assert.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.HashMap;
 
 public class ScrabbleTest {
 
@@ -14,5 +14,26 @@ public class ScrabbleTest {
         Scrabble testScrabble = new Scrabble();
         Integer expected = 1;
         assertEquals(expected, testScrabble.calculateScore("a"));
+    }
+
+    @Test
+    public void calculateScore_returnsScoreForAnotherSingleLetter_10() throws Exception {
+        Scrabble testScrabble = new Scrabble();
+        Integer expected = 10;
+        assertEquals(expected, testScrabble.calculateScore("q"));
+    }
+
+    @Test
+    public void calculateScore_returnsScoreForWord_5() throws Exception {
+        Scrabble testScrabble = new Scrabble();
+        Integer expected = 5;
+        assertEquals(expected, testScrabble.calculateScore("cat"));
+    }
+
+    @Test
+    public void calculateScore_returnsScoreForMixedCaseWord_5() throws Exception {
+        Scrabble testScrabble = new Scrabble();
+        Integer expected = 5;
+        assertEquals(expected, testScrabble.calculateScore("Cat"));
     }
 }
